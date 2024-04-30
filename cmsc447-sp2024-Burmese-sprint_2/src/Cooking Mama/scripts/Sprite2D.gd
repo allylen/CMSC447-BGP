139f13e4efad57d2fc61b9dabe7f6255f2a52ace
Michael Dickenson <mdicken1@umbc.edu>
2024-04-30 19:49:03 -0400

    Fixed scoring display

--- cmsc447-sp2024-Burmese-sprint_2/src/Cooking Mama/scripts/Sprite2D.gd
+++ cmsc447-sp2024-Burmese-sprint_2/src/Cooking Mama/scripts/Sprite2D.gd
@@ -9,8 +9,8 @@ var sprite_cut = 0 #start with no cuts
 var knife
 var knife_speed = 400
 var score = 0
-var score_label
-var score_popup
+var score_node
+var popup_node
 var score_popup_timer
 var screen_center_x = 960 #based on 1920x1080 screen
 var screen_center_y = 540 #based on 1920x1080 screen
@@ -19,6 +19,7 @@ var screen_min_x = 0
 var min_score = 20
 var max_click_distance = 206
 var vegetables_cut = 0
+var parent_node
 
 func _ready():
 	set_sprite(0) #start with first sprite
@@ -30,21 +31,13 @@ func _ready():
 	knife.position.y = screen_center_y - knife.texture.get_height()/2
 	knife.visible = true
 	
+	parent_node = get_parent() #get parent node
+	
 	#score label at top left
-	score_label = Label.new()
-	score_label.text = "Score: 0"
-	score_label.position = Vector2(-500,-400)
-	add_child(score_label)
+	score_node = parent_node.get_node("ScoreNumTotal")
 	
 	#popup for score
-	score_popup = Label.new()
-	score_popup.text = ""
-	score_popup.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	score_popup.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-	score_popup.visible = false
-	add_child(score_popup)
-	score_popup.position.x = screen_center_x
-	score_popup.position.y = screen_center_y
+	popup_node = parent_node.get_node("PopupNum")
 	
 func _process(delta):
 	#Move the knife back and forth
@@ -61,27 +54,28 @@ func _input(event):
 		if click_distance <= max_click_distance:
 			if get_rect().has_point(to_local(event.position)):
 				if sprite_cut < len(sprites[sprite_index]["cuts"]):
-					self.texture = sprites[sprite_index]["cuts"][sprite_cut]
 					sprite_cut += 1
+					self.texture = sprites[sprite_index]["cuts"][sprite_cut-1]
 					
 					var click_score = round(100 - (click_distance / max_click_distance) * (100 - min_score))
 					score += click_score
-					score_label.text = "Score: " + str(score)
 					
-					score_popup.text = "+" + str(click_score)
-					score_popup.visible = true
-					score_popup_timer = Timer.new()
-					score_popup_timer.connect("timeout", self, "hide_score_popup")
-					score_popup_timer.start(3.0)
+					popup_node.display_points(click_score) # do popup score
+					score_node.incr_points(click_score) # incr total score
+					popup_node.display_points(-1) #reset it to blank
+					
+					if sprite_cut == len(sprites[sprite_index]["cuts"]):
+						await get_tree().create_timer(0.5).timeout
+						next_sprite()
 				else:
 					vegetables_cut += 1
 					if vegetables_cut >=2:
 						get_tree().change_scene_to_file("res://scenes/cooking.tscn")
 					else:
 						next_sprite()
-func hide_score_popup():
-	score_popup.visible = false
-	score_popup_timer.stop()
+#func hide_score_popup():
+#	score_popup.visible = false
+#	score_popup_timer.stop()
 func set_sprite(index):
 	sprite_index = index
 	self.texture = sprites[index]["sprite"]
@@ -89,8 +83,11 @@ func set_sprite(index):
 	self.position.y = screen_center_y
 	sprite_cut = 0
 func next_sprite():
-	sprite_index = (sprite_index + 1) % sprites.size()
-	set_sprite(sprite_index)
+	sprite_index = sprite_index + 1
+	if sprite_index >= 2:
+		get_tree().change_scene_to_file("res://scenes/cooking.tscn")
+	else:
+		set_sprite(sprite_index)
 func get_level_speed(level):
 	return knife_speed * (level if level > 0 else 1)
 func _physics_process(delta):
