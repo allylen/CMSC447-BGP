ebeb54224b4562e38df30d3849d7c14134557ea4
Nikki Cayas <ncayas14@gmail.com>
2024-05-01 18:47:20 -0400

    implemented assmebling functionality for levels 1,2,3

---
+++ cmsc447-sp2024-Burmese-sprint_2/src/Cooking Mama/scenes/assemblingLevel2/playerMoveBun.gd
@@ -0,0 +1,13 @@
+# LEVEL 2 SCRIPT
+extends CharacterBody2D
+
+func _ready():
+	# connect event signal
+	set_process_input(true)
+
+func _physics_process(delta):
+	# player / bun movement
+	if Input.is_action_pressed("ui_right"):
+		position.x += 6
+	if Input.is_action_pressed("ui_left"):
+		position.x -= 6
---
+++ cmsc447-sp2024-Burmese-sprint_2/src/Cooking Mama/scenes/assemblingLevel2/topBunScript.gd
@@ -0,0 +1,51 @@
+extends Area2D
+
+var speed = 350 # Adjust the falling speed as needed
+var is_stopped = false
+var bottomBunNode = null
+var pointsNode = null
+var is_colliding = false
+var amount = 0
+var round_ended = false
+
+func _ready():
+	# get parent node
+	var parent_node = get_parent()
+	# get bun node
+	bottomBunNode = parent_node.get_node("bottomBunNode")
+	# get points node
+	pointsNode = parent_node.get_node("Points")
+	var random_x = randi_range(-500, 500)
+	position.x = random_x
+	set_process_input(true)
+
+func _process(delta):
+	if !is_colliding:
+		# burger move downwards until collision is met
+		position.y += speed * delta
+		if round_ended:
+			# end round by switching
+			end()
+	else:
+		# update post to follow bottom bun
+		position.x = bottomBunNode.position.x + amount
+
+func collision():
+	is_colliding = true
+	# calculate points & update accordingly using points node
+	amount = position.x - bottomBunNode.position.x
+	pointsNode.increment_points((int)((400 - abs(amount))/4))
+	
+func end():
+	# covers both winning and loss case
+	# switch scene to endRound scene to display updated points
+	get_tree().change_scene_to_file("res://scenes/assemblingLevel2/endRound.tscn")
+
+func _on_patty_node_10_area_entered(area):
+	collision()
+	# round won!
+	round_ended = true
+	end()
+	
+func _on_game_over_area_area_entered(area):
+	round_ended = true
